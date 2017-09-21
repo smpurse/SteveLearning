bc799e74fc6564e46cfed4d9cda281dbe440eea2
Steve Pursell <spursell@cox.net>
2017-09-21 14:39:19 -0700

    remove debugging trace

--- hmc588L1test.py
+++ hmc588L1test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# s.pursell use terminal command python3 hmc5883Ltest.py > outputfilename.dat to send output to a file
+# stevep:  use a terminal redirect from python3 hmc5883Ltest.py > outputfilename.dat to send output to a file
 
 import smbus
 import time
@@ -9,21 +9,16 @@ bus = smbus.SMBus(1)
 
 address = 0x1e
 
-#print ("I am here 1")
-
 def read_byte(adr):
     return bus.read_byte_data(address, adr)
-#    print ("I am read_byte")
     
 def read_word(adr):
-#    print ("I am read_word")
     high = bus.read_byte_data(address, adr)
     low = bus.read_byte_data(address, adr+1)
     val = (high << 8) + low
     return val
 
 def read_word_2c(adr):
-#    print ("I am read_word_2c")
     val = read_word(adr)
     if (val >= 0x8000):
         return -((65535 - val) + 1)
@@ -31,15 +26,13 @@ def read_word_2c(adr):
         return val
     
 def write_byte(adr, value):
-#    print ("I am write_byte")
     bus.write_byte_data(address, adr, value)
+
     
 #write_byte(0, 0b01110000) #Set to 8 samples @ 15Hz
 #write_byte(0, 0b00010000) #Set to 1 sample @ 15Hz
 write_byte(0, 0b00001000) #Set to 1 sample @ 3Hz
-
 write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
-
 write_byte(2, 0b00000000) # Continuous sampling
 
 scale = 0.92
@@ -51,8 +44,6 @@ for i in range(0,500):
 
      bearing = math.atan2(y_out, x_out)
     
-     #print ("I am here 2")
-
      if (bearing < 0):
          bearing += 2 * math.pi
        
@@ -67,4 +58,4 @@ for i in range(0,500):
      
 print ("Finished with 500 samples")
 
-#print ("I am at the end")  
+
